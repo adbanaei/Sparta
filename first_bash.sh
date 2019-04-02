7e4f361e11f7ee713af0448e136b5d4296875f43
adbanaei <48990617+adbanaei@users.noreply.github.com>
2019-04-02 23:31:53 +0430

    Update first_bash.sh

--- first_bash.sh
+++ first_bash.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # My first script
-
+##################################update
 yum -y update
 yum -y install epel-release nano git firewalld
 yum -y groupinstall "Development Tools"
+##################################firewall
 systemctl start firewalld
 systemctl enable firewalld
 systemctl status firewalld
+######################################ssh
 mkdir -p ~/.ssh
 echo "Please enter your public key for your secure SSH connection:"
 read -p "(Enter it without ssh-rsa):" sshauthkey
@@ -20,7 +22,15 @@ sed -i 's/.*Port 22.*/Port $cussshport/g' /etc/ssh/sshd_config
 firewall-cmd --zone=public --add-port=$cussshport/tcp --permanent
 firewall-cmd --reload
 systemctl restart sshd
+##################################hostname
 echo "Please enter your hostname:"
 read -p "(Something like: ns.domain.tld):" hn
 hostname $hn
-hostname
+###################################vpnserver
+cd /opt
+git clone https://github.com/SoftEtherVPN/SoftEtherVPN_Stable.git
+cd SoftEtherVPN
+make
+make install
+./vpnserver start
+####################################proxyserver
