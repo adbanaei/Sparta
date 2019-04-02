0009961e7c7d38ed42713b003f5e8d1d153b0099
adbanaei <48990617+adbanaei@users.noreply.github.com>
2019-04-02 23:13:03 +0430

    Update first_bash.sh

--- first_bash.sh
+++ first_bash.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 # My first script
 
-yum update -y
-df -h
-yum install nano
+yum -y update
+yum -y install epel-release nano git firewalld
+yum -y groupinstall "Development Tools"
+systemctl start firewalld
+systemctl enable firewalld
+systemctl status firewalld
 mkdir -p ~/.ssh
 echo "Please enter your public key for your secure SSH connection:"
 read -p "(Enter it without ssh-rsa):" sshauthkey
 echo ssh-rsa $sshauthkey >> ~/.ssh/authorized_keys
 chmod -R go= ~/.ssh
 chown -R root:root ~/.ssh
-nano /etc/ssh/sshd_config
+echo "Please enter the port that you want to connect to SSH with:"
+read -p "(Default Port: 22511 ):" cussshport
+[ -z "${cussshport}" ] && cussshport="22511"
+sed -i 's/.*Port 22.*/Port $cussshport/g' /etc/ssh/sshd_config
+firewall-cmd --zone=public --add-port=$cussshport/tcp --permanent
+firewall-cmd --reload
 systemctl restart sshd
-hostname sparta.mlsrp.ir
+echo "Please enter your hostname:"
+read -p "(Something like: ns.domain.tld):" hn
+hostname $hn
 hostname
